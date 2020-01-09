d304e265b9057394e1d652527d861342a4603cdf
b1ueb1ues <bblues@live.cn>
2020-01-10 02:44:16 +0800

    update

---
+++ old/event.js
@@ -0,0 +1,116 @@
+
+function type(a){
+    return a.__proto__.constructor.name;
+}
+
+class Event_list {
+    constructor() {
+        this._event_listeners = [];
+    }
+
+    add_event_listener(eventname, listener){
+        if (eventname in this._event_listeners) {
+            this._event_listeners[eventname].push(listener); }
+        else {
+            this._event_listeners[eventname] = [listener]; }
+    }
+
+    get_event_trigger(eventname) {
+        if (eventname in this._event_listeners) {
+            return this._event_listeners[eventname]; }
+        else {
+            this._event_listeners[eventname] = [];
+            return this._event_listeners[eventname]; }
+    }
+} // class Event_list
+
+export class Event {
+    constructor(name) {
+        if (name) {
+            this.name = name;
+            this._trigger = ctx.active_el.get_event_trigger(name); }
+        else {
+            this._trigger = []; }
+    }
+
+    listener(cb) {
+        if (type(cb) == 'Array'){
+            for (var i in cb) {
+                i = cb[i];
+                ctx.active_el.add_event_listener(this.name, i); } }
+        else {
+            ctx.active_el.add_event_listener(this.name, cb); } 
+    }
+
+    on(e) {
+        for (var i in this._trigger) {
+            i = this._trigger[i];
+            i(this); }
+    }
+
+    init() {
+        new Event_list();
+    }
+} // class Event
+
+class Listener {
+    constructor(name, cb){
+        this.__cb = cb
+        this.__eventname = name
+        this.__online = 0
+        this.on()
+    }
+
+    on(cb) {
+        if (this.__online) {
+            return; }
+        if (cb) {
+            this.__cb = cb; }
+
+        if (type(this.__eventname) == 'Array') {
+            for (var i in this.__eventname) {
+                i = this.__eventname[i];
+                ctx.active_el.add_event_listener(i, this.__cb); } }
+        else {
+            ctx.active_el.add_event_listener(this.__eventname, this.__cb); }
+        this.__online = 1;
+        return this;
+    }
+
+    off() {
+        if (!this.__online) {
+            return; }
+        if (type(this.__eventname) == 'Array') {
+            for (var i in this.__eventname) {
+                i = this.__eventname[i];
+                var els = ctx.active_el.get_event_trigger(i);
+                var idx = els.indexOf(this.__cb);
+                els.splice(idx, 1); } }
+        else {
+            var els = ctx.active_el.get_event_trigger(this.__eventname);
+            var idx = els.indexOf(this.__cb);
+            els.splice(idx, 1); }
+        this.__online = 0;
+        return this;
+    }
+}
+
+function teste() {
+    function foo(e) {
+        print('foo: '+e.name); 
+        print(': '+e.test); 
+    }
+    function bar(e) {
+        print('bar: '+e.name); 
+    }
+
+    new Event().init();
+    e = new Event('test');
+    e.test = 1;
+    l = new Listener('test', foo);
+    l2 = new Listener(['test2','test'], bar);
+    e2 = new Event('test2');
+    e.on();
+    e2.on();
+}
+teste();
---
+++ old/log.js
@@ -0,0 +1,91 @@
+var loglevel = 0;
+
+function log(t, name, amount, misc) {
+    ctx.active_log.push([now(), t, name, amount, misc]);
+}
+
+class Log {
+    constructor() {
+    }
+    init(log) {
+        if (log) {
+            ctx.active_log = log;
+            this.active = ctx.active_log;
+            return 1; }
+        else {
+            ctx.active_log = [];
+            this.active = ctx.active_log;
+            return ctx.active_log; }
+    }
+    log(log) {
+        if (!log) {
+            log = this.active;
+        }
+        log.push([now(), t, name, amount, misc]);
+    }
+    catline(i) {
+        if (!i) {
+            i = this.active[0];
+        }
+        var j = [];
+        j[1] = i[1];
+        j[2] = i[2];
+        j[3] = i[3];
+        j[4] = i[4];
+
+        if (!i[1]) {
+            j[1] = ''; }
+        else {
+            j[1] = i[1]; }
+
+        if (!i[2]) {
+            j[2] = ''; }
+        else {
+            j[2] = i[2]; }
+
+        if (!i[3]) {
+            j[3] = ''; }
+        else {
+            j[3] = i[3]; }
+
+        if (!i[4]) {
+            j[4] = ''; }
+        else {
+            j[4] = i[4]; }
+
+        print(''+i[0]+': '+j[1]+'\t, '+j[2]+'\t, '+j[3]+'\t, '+j[4]+'') 
+    }
+}
+
+
+function logcat(filter, log) {
+    if (!log) {
+        log = ctx.active_log; }
+    l = new Log();
+
+
+    if (!filter) {
+        for (var i in log) {
+            l.catline(log[i]);
+        } 
+    } 
+    else {
+        if (typeof filter == 'string'){
+            filter = [filter];
+        }
+        for (var i in log) {
+            for (var j in filter) {
+                if (log[i][1] == filter[j]) {
+                    l.catline(log[i]); } } 
+        } 
+    }
+}
+
+function logget() {
+    return ctx.active_log;
+}
+
+function logreset() {
+    ctx.active_log = [];
+}
+
