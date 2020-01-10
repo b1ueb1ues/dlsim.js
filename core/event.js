d7aaacc32f1cfb5c1a761c66fc4aa340621e8450
b1ueb1ues <bblues@live.cn>
2020-01-11 01:39:23 +0800

    update

--- core/event.js
+++ core/event.js
@@ -1,9 +1,14 @@
 
-class _Event {
-    constructor(el, name) {
-        this._el = el;
+export class Event {
+    constructor(event_listeners, name) {
+        this._el = event_listeners;
         this._name = name;
-        this._trigger = el.get_event_trigger(name);
+        if (name in event_listeners) {
+            this._trigger = event_listeners[name]; }
+        else {
+            this._trigger = [];
+            event_listeners[name] = this._trigger;
+        }
     }
     on() {
         for (var i in this._trigger){
@@ -12,13 +17,20 @@ class _Event {
         return this;
     }
     listener(callback) {
-        let l = new _Listener(this._el, this._name, callback);
+        let l = new Listener(this._el, this._name, callback);
         l.on();
         return l;
     }
+    static init() {
+        let el={};
+        function new_event(name) {
+            return new Event(el, name);
+        }
+        return new_event;
+    }
 }
 
-class _Listener {
+class Listener {
     constructor(el, name, callback) {
         this._el = el;
         this._name = name;
@@ -31,44 +43,21 @@ class _Listener {
             errrrrrrrr();
             return;
         }
-        this._el.add_event_listener(this._name, this._cb);
+        if (this._name in this._el) {
+            this._el[this._name].push(this._cb); }
+        else {
+            this._el[this._name] = [this._cb]; }
         this._online = 1;
         return this;
     }
     off() {
         if (!this._online)  // acceptable
             return 0;
-        let et = this._el.get_event_trigger(this._name);
+        let et = this._el[this._name];
         let idx = et.indexOf(this._cb);
         et.splice(idx, 1);
         this._online = 0;
         return this;
-    }
-}
-
-
-class EventL {
-    constructor() {
-        this._event_listeners = {};
-        let _el = this;
-        this.Event = function(name) {
-            return new _Event(_el, name);
-        }
-    }
-
-    add_event_listener(eventname, callback){
-        if (eventname in this._event_listeners) {
-            this._event_listeners[eventname].push(callback); }
-        else {
-            this._event_listeners[eventname] = [callback]; }
-    }
-
-    get_event_trigger(eventname) {
-        if (eventname in this._event_listeners) {
-            return this._event_listeners[eventname]; }
-        else {
-            this._event_listeners[eventname] = [];
-            return this._event_listeners[eventname]; }
     }
 }
 
