2bee5236db2af87d63dda34126afec1f9c7f9ff4
b1ueb1ues <bblues@live.cn>
2020-04-29 18:55:24 +0800

    save

--- base/action.js
+++ base/action.js
@@ -54,8 +54,9 @@ export class Action {
         this.delay = conf.delay;
         this.marker = conf.marker;
         this.duration = conf.duration;
-        this.proc = conf.proc;
-        this.firstblood = 0;
+        if (conf.proc)
+            this.proc = conf.proc;
+        this.firstblood = 1;
         this.speed = 1;
         this.soft_next = null;
         this.hard_next = null;
@@ -65,18 +66,21 @@ export class Action {
         this.tk_end = Tick(this.duration, this.on_end);
         this.ticks = [this.tk_end];
 
-        this.regist();
+        this.regist_start();
         this.add_tick(conf.hit, this.on_hit);
         this.add_tick(conf.cancel, this.on_cancel);
         this.add_tick(conf.hook, this.on_hook);
         this.add_tick(conf.buff, this.on_buff);
-        this.regist_();
+        this.regist_end();
     }
 
-    regist() {
+    regist_start() {
         let action = this;
         this.on_hit = function(conf) {
             console.log(`${now().toFixed(3)}: hitlabel_${conf.hit}`);
+            if (action.firstblood) {
+                action.firstblood = 0;
+            }
         }
         this.on_cancel = function(conf) {
             console.log(`${now().toFixed(3)}: canceldata_${conf.cancel}`);
@@ -87,7 +91,7 @@ export class Action {
         this.on_buff = function(conf) {
         }
     }
-    regist_() {
+    regist_end() {
         if (this.ticks.length >= 2) {
             for (var i=1; i<this.ticks.length; i++) {
                 this.ticks[i-1].next = this.ticks[i];
@@ -96,6 +100,7 @@ export class Action {
     }
 
     on() { 
+        this.firstblood = 1;
         if (!this.ctx.active) {
             this.ticks[this.tick_next].start_at(0, this.ctx.speed);
         }
@@ -137,12 +142,12 @@ export class Action {
     }
 
     farsee() { }
-
+    proc() { }
 }
 
 let conf_x = {"atype":"x", "input":0.1, "duration":3, "proc":null};
 let conf_s = {"atype":"s", "input":0.1, "duration":3, "proc":null};
-let conf_fs = {"atype":"fs", "marker":0.15, "input":0.15, "duration":3, "proc":null};
+let conf_fs = {"atype":"fs", "input":0.15, "marker":0.15,"duration":3, "proc":null};
 
 let conf = conf_x;
 conf.hit = [
