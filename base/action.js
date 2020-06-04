cc653deda87894d0e7342c93dfdd4b9e3ef2a4dd
b1ueb1ues <bblues@live.cn>
2020-06-04 17:16:33 +0800

    save

--- base/action.js
+++ base/action.js
@@ -1,5 +1,7 @@
 import {now, Timer} from '../core/ctx.js'
 import * as ctx from '../core/ctx.js'
+import {print, dprint} from '../core/print.js'
+import {Logger, logset} from '../core/log.js'
 
 
 
@@ -10,7 +12,7 @@ function Tick(...args) {
 class _Tick {
     constructor(t, on_tick, data) {
         let tk = this;
-        console.log(on_tick);
+        console.log('on_tick',on_tick);
         this.data = data;
         this._next = {};
         this._next.start_at = function(){};
@@ -41,7 +43,9 @@ class _Tick {
 
 export class Action {
     static init(src) {
-        let ctx = {"speed":1, "active":null, "next":null};
+        let nop = {};
+        nop.cancel_data = {'x':1, 's':1, 'fs':1};
+        let ctx = {"speed":1, "active":nop, "next":null, "t_input":null, "t_marker":null, "nop":nop};
         function new_action (conf) {
             return new Action(ctx, conf);
         }
@@ -49,7 +53,7 @@ export class Action {
     }
     constructor(ctx, conf) {
         this.ctx = ctx;
-        this.state = -1; // -1:pre 0:act 1:end
+        this.state = 2; // -3:input -2:marker -1:ready 0:act 1:end
         this.conf = conf;
         this.atype = conf.atype;
         this.duration = conf.duration;
@@ -61,12 +65,16 @@ export class Action {
         this.soft_next = null;
         this.hard_next = null;
 
-        this.cancel = null;
-        this.input = null;
+        this.cancel_data = {};
+        this.input_data = {};
 
         this.tick_next_idx = 0;
-        this.tk_end = Tick(this.duration, this.on_end);
-        this.ticks = [this.tk_end];
+        if (this.duration) {
+            this.tk_end = Tick(this.duration, this.on_end);
+            this.ticks = [this.tk_end];
+        } else {
+            this.ticks = [];
+        }
 
         this.regist_start();
         this.add_tick(conf.input, this.on_input);
@@ -76,7 +84,7 @@ export class Action {
         this.add_tick(conf.cancel, this.on_cancel);
         this.regist_end();
         this.hit_total = conf.hit.length;
-        console.log(this.hit_total);
+        console.log('hit_total',this.hit_total);
     }
 
     regist_start() {
@@ -93,7 +101,10 @@ export class Action {
         }
         this.on_buff = function(conf) {
         }
+        this.on_input = function(conf) {
+        }
     }
+
     regist_end() {
         if (this.ticks.length >= 2) {
             for (var i=1; i<this.ticks.length; i++) {
@@ -102,72 +113,51 @@ export class Action {
         }
     }
 
-    next(a_next) {
-        if (a_next.conf.input) {
-
-        }
-    }
-
     tap() {
-        this.state = -2;
-    }
-
-
-    start_input() {  
-        if (this.atype in this.ctx.active.input) { // must have an active action
-            if this.input
-            Timer(function (){
-
-            }).on(this.input)
-
+        // must have an active action
+        if (this.ctx.next) {
+            return;
+        } else {
+            this.ctx.next = this;
         }
-
+        
+        if (this.conf.input) {  
+            if (this.ctx.active.input_data[this.atype]){
+                let host = this;
+                this.state = -3;
+                this.ctx.t_input = Timer(function(){
+                    host.ctx.t_input = null;
+                    if (host.conf.marker) {  
+                        host.start_marker();
+                    } else {
+                        this.state = -1;
+                    }
+                }).on(this.conf.input);
+            }
+        }
     }
-    start_marker() {
 
+    start_marker() {
+        if (this.ctx.active.input_data['marker']) {
+            this.ctx.t_marker = Timer(function(){
+                this.state = -1;
+                host.ctx.t_marker = null;
+                this.on();
+            }).on(this.conf.marker);
+        }
     }
     
-    act() {
-        this.state = 0;
-        this.hit_cur = 0;
-        if (this.nospeed)
-            this.ticks[this.tick_next_idx].start_at(0);
-        else
-            this.ticks[this.tick_next_idx].start_at(0, this.ctx.speed);
-    }
-
     on() { 
-        if (this.check) {
-            if (this.nospeed)
-                this.ticks[this.tick_next_idx].start_at(0);
-            else
-                this.ticks[this.tick_next_idx].start_at(0, this.ctx.speed);
+        if (this.ctx.active.cancel_data[this.atype]) {
+            this.state = 0;
         }
     }
+
     off() { }
 
-    pre() {
-        if (this.input) {
-
-        }
-
-    }
 
     on_end(t) {
     }
-
-    check() {
-        if (!this.ctx.active) {
-            return 1;
-        }
-        if (this.atype in this.ctx.active.cancel) {
-            return 1;
-        }
-        if (this.itype in this.ctx.active.input) {
-            return 1;
-        }
-    }
-
 
     add_tick(conf, callback) {
         let action = this;
